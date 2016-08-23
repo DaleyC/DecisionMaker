c8dfbb36ea415282cd67ce483bc1d89d2a3ed6f7
Daley <ctlncardwell@gmail.com>
2016-08-23 13:52:07 -0400

    feat(app): set up functional environment for React

---
+++ src/app.js
@@ -0,0 +1,6 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Base = require('./components/base');
+
+ReactDOM.render(<Base />, document.getElementById('app'));
---
+++ src/components/base.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var React = require('react');
+
+var Base = React.createClass({
+	render: function () {
+		return <h1>What should we eat?</h1>;
+	}
+});
+
+module.exports = Base;
